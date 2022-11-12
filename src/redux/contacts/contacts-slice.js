cfba351eb70985c75e21302045fac3c5f25c2e55
Antonyly <antonlyzun@gmail.com>
2022-11-12 08:33:19 +0200

    +

---
+++ src/redux/contacts/contacts-slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
+
+export const initialState = {
+  items: [],
+  filter: '',
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: initialState.items,
+  reducers: {
+    add: {
+      reducer(store, { payload }) {
+            return [payload, ...store];
+          },
+      prepare(data) {
+          return {
+              payload: { ...data, id: nanoid() },
+          };
+          },
+    },
+    remove: (state, { payload }) => {
+        return state.filter(el => el.id !== payload);
+      },
+    },
+});
+export const { add, remove } = contactsSlice.actions;
+export default contactsSlice.reducer;
